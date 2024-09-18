039f5d18beefebae8172553ef606ca656b445dd2
juniorjaviersanchez <juniorsc750@gmail.com>
2024-09-18 00:05:48 -0500

    Update

--- src/modules/finance/layouts/FinanceLayout.vue
+++ src/modules/finance/layouts/FinanceLayout.vue
@@ -1,11 +1,34 @@
 <template>
-    
-    <!---<v-progress-circular indeterminate></v-progress-circular> -->
-
-    <NavbarComponent />
-    <div>
-        <router-view />
+    <div v-if="loading" class="d-flex justify-center align-center fill-height">
+        <div class="mx-auto text-center">
+            <v-progress-linear
+              indeterminate
+              :width="15"
+              color="primary"
+              size="50"
+            ></v-progress-linear>
+            <v-img
+                src="@/assets/img/first/javier-logo-color.png"
+                width="200"
+                height="100"
+                cover
+                class="mx-auto"
+            ></v-img>
+            <v-progress-linear
+              indeterminate
+              :width="15"
+              color="primary"
+              size="50"
+            ></v-progress-linear>
+        </div>
     </div>
+
+    <template v-else>
+        <NavbarComponent />
+        <div>
+            <router-view />
+        </div>
+    </template>
 </template>
   
 <script>
@@ -14,11 +37,20 @@
 
     export default {
         name: 'FinanceLayout',
+        data() {
+            return {
+                loading: true,
+            };
+        },
         components: {
             NavbarComponent: defineAsyncComponent(() => import(/* webpackChunkName: "Navbar" */ '@/modules/finance/components/NavbarComponent.vue'))
         },
         mounted() {
-            document.title = 'Welcome | Finanzas'
+            document.title = 'Welcome | Finanzas';
+
+            setTimeout(() => {
+                this.loading = false; 
+            }, 1000);
         }
     }
 
