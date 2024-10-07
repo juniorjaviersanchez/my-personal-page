5d68e670d3f47312a1e7bc85a09c9207f05d9156
juniorjaviersanchez <juniorsc750@gmail.com>
2024-10-06 22:27:54 -0500

    update porfolio

--- src/modules/myWeb/views/PortfolioView.vue
+++ src/modules/myWeb/views/PortfolioView.vue
@@ -77,7 +77,7 @@
                   target="_blank"
                 >
                   Ver 
-                  <v-icon icon="mdi-cursor-default-click-outline"></v-icon>
+                  <v-icon :icon="project.iconPage?project.iconPage:'mdi-cursor-default-click-outline'"></v-icon>
                 </v-btn>
               </v-card-text>
             </v-card>
@@ -96,6 +96,14 @@ export default {
     return {
       projects: [
         {
+          title: 'Appdora',
+          description: 'Este proyecto es una SPA (Single Page Application) que utiliza Vue.js y Vuetify para la interfaz de usuario, con Axios para realizar peticiones al backend. Laravel se encarga de la lógica del servidor y proporciona una API para gestionar el seguimiento de participantes en un evento. SweetAlert2 se utiliza para mostrar alertas personalizadas.',
+          image: require("@/assets/img/portfolio/project-appdora.webp"),
+          url: '',
+          tags: ['Vue', 'Vuetify', 'Laravel', '2024'],
+          iconPage: 'mdi-lock'
+        },
+        {
           title: 'Conversor de moneda',
           description: 'Este proyecto utiliza Vue.js para construir la interfaz de usuario, Vuetify para los componentes estilizados, y Axios para realizar peticiones a una API de tasas de cambio. Además, SweetAlert2 se emplea para mostrar alertas personalizadas.',
           image: require("@/assets/img/portfolio/project-currency-converter.webp"),
@@ -104,7 +112,7 @@ export default {
         },
         {
           title: 'Piedra, papel o tijera',
-          description: 'Este proyecto utiliza Vue.js para construir la interfaz de usuario, Vuetify para los componentes estilizados. Es un juego interactivo de Piedra, Papel o Tijera, donde los usuarios pueden competir contra la computadora.',
+          description: 'Es un juego interactivo de Piedra, Papel o Tijera, donde los usuarios pueden competir contra la computadora. Este proyecto utiliza Vue.js para construir la interfaz de usuario, Vuetify para los componentes.',
           image: require("@/assets/img/portfolio/rock-scissors-paper.webp"),
           url: 'https://javier-project-n3.netlify.app/',
           tags: ['Vue', 'Vuetify', '2024'],
@@ -122,6 +130,7 @@ export default {
           image: require("@/assets/img/portfolio/spartanbeer.webp"),
           url: '',
           tags: ['HTML', 'CSS', '2023'],
+          iconPage: 'mdi-lock'
         },
         {
           title: 'Ceconp',
@@ -134,8 +143,9 @@ export default {
           title: 'CPIS - FICSA',
           description: 'Un sistema de mátricula, pagos y contabilidad de cursos.',
           image: require("@/assets/img/portfolio/cpis-sistema.webp"),
-          url: 'https://cpisficsa.com/',
+          url: '',
           tags: ['Laravel', 'CSS', 'TailwindCSS', '2022'],
+          iconPage: 'mdi-lock'
         },
       ]
     };
